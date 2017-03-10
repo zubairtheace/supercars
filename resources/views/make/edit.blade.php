a490512a2c012ce4deac27d75c230ca981d3feb8
Tofy Zubair <tofy.zubair@gmail.com>
2017-03-10 22:37:15 +0400

    car make CRUD

--- resources/views/make/edit.blade.php
+++ resources/views/make/edit.blade.php
@@ -5,15 +5,14 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Add New Car Make</div>
+                <div class="panel-heading">Edit Car Make</div>
                 <div class="panel-body">
                     {!! Form::open(
                         array(
-                            'route' => 'make.store',
+                            'route' => ['make.update', $make->id],
                             'class' => 'form-horizontal',
                             'role' => 'form',
-                            'method' => 'POST',
-                            'files' => true)) !!}
+                            'method' => 'put')) !!}
 
                     @if (count($errors) > 0)
                     <div class="alert alert-danger">
@@ -33,10 +32,11 @@
                             )) !!}
 
                         <div class="col-md-6">
-                            {!! Form::text('make',null,
+                            {!! Form::text('make',$make->make,
                                 array(
                                     'class'=>'form-control',
-                                    'required' => 'required'
+                                    'required' => 'required',
+                                    'autofocus' => 'autofocus'
                                 )) !!}
                         </div>
                     </div>
@@ -51,31 +51,7 @@
                         </div>
                     </div>
                     {!! Form::close() !!}
-                    <!-- <form class="form-horizontal" role="form" method="POST" action="{{ route('make.store') }}">
-                        {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('make') ? ' has-error' : '' }}">
-                            <label for="make" class="col-md-4 control-label">Make</label>
-
-                            <div class="col-md-6">
-                                <input id="make" type="text" class="form-control" name="make" value="{{ old('make') }}" autofocus>
-
-                                @if ($errors->has('make'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('make') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Save
-                                </button>
-                            </div>
-                        </div>
-                    </form> -->
                 </div>
             </div>
         </div>
--- resources/views/make/index.blade.php
+++ resources/views/make/index.blade.php
@@ -5,11 +5,34 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Car Makes</div>
-
+                <div class="panel-heading">Car Makes
+                  <a href="{{ route('make.create') }}" class="btn btn-primary btn-sm pull-right">Add </a>
+                </div>
                 <div class="panel-body">
-                    Car Makes listing will be displayed here.
-
+                  <table class="table">
+                    @forelse($makes as $make)
+                      <tr>
+                        <td>
+                          <b>name:</b> {{$make->make}}
+                          <div class="btn-toolbar pull-right">
+                            <div class="btn-group">
+                              <a href="{{ route('make.show', $make->id) }}" class="btn btn-primary btn-sm">View </a>
+                            </div>
+                            <div class="btn-group">
+                              <a href="{{ route('make.edit', $make->id) }}" class="btn btn-primary btn-sm">Edit </a>
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    @empty
+                    <tr>
+                      <td>No Make </td>
+                    </tr>
+                    @endforelse
+                    </table>
+                    <div>
+                        {{ $makes->links() }}
+                    </div>
                 </div>
             </div>
         </div>
